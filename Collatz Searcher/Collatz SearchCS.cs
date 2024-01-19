d4ee34068d7c72d5298b0bd678ed522682f84544
Mohamed Yasser <tecyouth123@gmail.com>
2024-01-20 01:39:58 +0200

    Collatz unique patterns searcher

---
+++ Collatz Searcher/Collatz SearchCS.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collatz_Searcher
+{
+    public class Collatz_SearchCS
+    {
+
+        const double factor = 0.584;
+        public int reached = 0;
+        long nodes = 0;
+        int directfordecrease=0;
+        public List<int> listdirectfordecrease = new List<int>();
+        public void Report(int maxi)
+        {
+            for(int i = 2; i <= maxi; i++)
+            {
+                int[] combination = new int[i+1];
+                directfordecrease = 0;
+                Generate(combination, i, 0, 0, 0);
+                listdirectfordecrease.Add(directfordecrease);
+                reached = i;
+                Console.WriteLine("Reached: "+i+" Combinations found: "+combinationsfound.Count+" ,Nodes: "+(nodes/1000000000.0).ToString("###########.00000000000000")+" 10^9");
+            }
+        }
+        public List<int[]> combinationsfound=new();
+        public List<int> lengthes=new ();
+        public List<int> vlcritics=new ();
+        bool Generate(int[] combination,int valCritic,int sumn,int sumz,int pointer)
+        {
+            double valn =(sumn * factor);
+            if (sumz + sumn == valCritic) {
+                if(valn - sumz <= -0.58)
+                {
+                    directfordecrease++;
+                }
+                if (valn - sumz <= 0&&valn-sumz+1>=0)
+                {
+                    combinationsfound.Add((int[])combination.Clone());
+                    lengthes.Add(pointer);
+                    vlcritics.Add(valCritic);
+                    nodes++;
+                    return true;
+                }
+                else
+                {
+                    nodes++;
+                    return false;
+                }
+            }
+            else if (pointer >= valCritic|| (valn-sumz<=0&&pointer>0))
+            {
+                nodes++;
+                return false;
+            }
+            if (pointer % 2 == 0)
+            {
+                //if(pointer >= valCritic - 1)
+                //{
+                //    return false;
+                //}
+                //test all n
+                int maxn = valCritic - sumn;
+                for (int n = 1; n <= maxn; n++)
+                {
+                    combination[pointer] = n;
+                    Generate(combination, valCritic, sumn + n, sumz, pointer + 1);
+                }
+
+            }
+            else
+            {
+                double maxz =(int)(valCritic - (valn - sumz));
+                for (int z=1;z<= maxz; z++)
+                {
+                    combination[pointer] = z;
+                    Generate(combination, valCritic, sumn, sumz + z, pointer + 1);
+                    //if(!a&&pointer>=valCritic-1)
+                    //{
+                    //    break;
+                    //}
+                }
+            }
+            return false;
+        }
+    }
+    struct ReportCL
+    {
+        public int[] combination;
+        public int frequency;
+    }
+}
---
+++ Collatz Searcher/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+
+namespace Collatz_Searcher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Collatz_SearchCS collatz_Searcher = new ();
+            int max = 21;
+            Stopwatch sp=Stopwatch.StartNew();
+            collatz_Searcher.Report(max);
+            sp.Stop();
+            Console.WriteLine(sp.ElapsedMilliseconds+"");
+            Console.WriteLine("Combinations found: "+collatz_Searcher.combinationsfound.Count);
+            Dictionary<int, int> valcritics = new();
+            valcritics.Add(1, 1);
+            for(int i=0;i<collatz_Searcher.combinationsfound.Count;i++)
+            {
+                string sl = "";
+                for(int j = 0; j < collatz_Searcher.lengthes[i]; j++)
+                {
+                    sl += $"{collatz_Searcher.combinationsfound[i][j]}+";
+                }
+                sl = sl.Trim('+');
+                int vlc = collatz_Searcher.vlcritics[i];
+                if (valcritics.ContainsKey(vlc))
+                {
+                    valcritics[vlc] += 1;// collatz_Searcher.lengthes[i]/2;
+                }
+                else
+                {
+                    valcritics.Add(vlc, 1);// collatz_Searcher.lengthes[i] / 2);
+                }
+               // Console.WriteLine($"{i+1} - Combination: 2^{vlc}, [{sl}]");
+            }
+            double totalpercentage = 0;
+            for(int i = 1; i <= max; i++)
+            {
+                int found = 0;
+                int foundeasydirect = 0;//
+                if (i > 1)
+                { foundeasydirect= collatz_Searcher.listdirectfordecrease[i-2]; }
+                if (valcritics.ContainsKey(i))
+                {
+                    found = valcritics[i];
+                }
+                totalpercentage += Math.Pow(2, -i) * found;
+                Console.Write($"2^{i}, Unique combinations found: {found}, direct: {foundeasydirect}");//direct means (Can have higher n values and still decrease)
+                Console.Write(" Percentage: "+totalpercentage+", Empty rooms: "+(Math.Pow(2,i)) *(1-totalpercentage)+"\r\n");
+            }
+            Console.WriteLine("Total percentage: "+totalpercentage);
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
